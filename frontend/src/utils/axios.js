e7df089dcb8d2f3bfd653203d220416707e41097
Prince.527 <110556348+adushdsauc@users.noreply.github.com>
2025-05-27 12:17:48 -0400

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
-  withCredentials: true, // ✅ this is required
+withCredentials: true, // this is required
     console.log("🔥 Base URL:", process.env.REACT_APP_API_URL);
 });
 
