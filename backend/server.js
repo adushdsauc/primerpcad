ce67f7caf28e9c28e59ec6b11343108a12d27779
Prince.527 <110556348+adushdsauc@users.noreply.github.com>
2025-05-27 12:51:17 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -64,8 +64,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: false,
+      sameSite: "lax",
     },
   })
 );
